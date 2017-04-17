890c18751dac96c7dd79c6937418a3014604feaa
bigezhang <894239925@qq.com>
2017-04-17 22:02:47 +0800

    change rem.js

--- src/config/rem.js
+++ src/config/rem.js
@@ -1,25 +1,17 @@
-(function () {
-  var html = document.documentElement
-  function onWindowResize () {
-    html.style.fontSize = html.getBoundingClientRect().width / 20 + 'px'
-  }
-  window.addEventListener('resize', onWindowResize)
-  onWindowResize()
-})()
-
-/*(function (doc, win) {
+(function (doc, win) {
   var docEl = doc.documentElement
   var resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize'
   var recalc = function () {
     var clientWidth = docEl.clientWidth
     if (!clientWidth) return
     if (clientWidth >= 640) {
-      docEl.style.fontSize = '1rem'
+      docEl.style.fontSize = '20px'
     } else {
-      docEl.style.fontSize = 1 * (clientWidth / 640) + 'rem'
+      docEl.style.fontSize = clientWidth / 32 + 'px'
     }
   }
   if (!doc.addEventListener) return
   win.addEventListener(resizeEvt, recalc, false)
   doc.addEventListener('DOMContentLoaded', recalc, false)
-})(document, window)*/
+})(document, window)
+
--- src/module/index/App.vue
+++ src/module/index/App.vue
@@ -4,54 +4,13 @@
 </template>
 
 <script>
-import Utils from '../../libs/utils'
-const _wx = window._wx
-export default {
-  data: function () {
-    return {
-      param: {
-        count: 1,
-        sizeType: ['original', 'compressed'],
-        sourceType: ['album', 'camera']
-      },
-      openid: Utils.getQueryString('openid'),
-      hideParam: ['menuItem:share:appMessage', 'menuItem:share:timeline', 'menuItem:share:qq', 'menuItem:share:weiboApp', 'menuItem:share:facebook', 'menuItem:share:QZone', 'menuItem:copyUrl', 'menuItem:openWithQQBrowser', 'menuItem:openWithSafari', 'menuItem:share:email', 'menuItem:share:brand']
-    }
-  },
-  beforeCreate () {
-    if (!_wx.isWx) {
-      window.location.href = 'error.html'
-    }
-  },
-  created () {
-    _wx.hideMenus(this.hideParam)
-  },
-  computed: {
-    href0 () {
-      return 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx33b31f7b21a1d52d&redirect_uri=http%3A%2F%2Fzhushou.weixin.chaojijiaolian.cn%2Findex%2Fdetail%3Fstate%3D%26supersign%3D%26cardmodel%3D%26appversion%3D&response_type=code&scope=snsapi_base&state=#wechat_redirect'
-    },
-    href1 () {
-      return `tposter.html?openid=${this.openid}`
-    },
-    href2 () {
-      return `uposter.html?openid=${this.openid}`
-    }
-  },
-  methods: {
-    goTpl0 () {
-      Utils.countBaidu('教练海报', 'click', '基础模板', '')
-    },
-    goTpl1 () {
-      Utils.countBaidu('教练海报', 'click', '学车好时节', '')
-    },
-    goTpl2 () {
-      Utils.countBaidu('教练海报', 'click', '再不学车就晚了', '')
-    }
-  },
-  components: {}
-}
 </script>
 
 <style lang="scss">
 @import "../../assets/scss/main.scss";
+.page {
+  width: 10rem;
+  height: 10rem;
+  background-color: #e77;
+}
 </style>
